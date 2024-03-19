dd7aa85c1debcb3f86b7437937d530fbef91b585
akifzdemir <akifz2199@gmail.com>
2024-03-19 11:23:05 +0300

    feat: add cors

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"example.com/go-htmx/config"
 	"example.com/go-htmx/controllers"
 	"example.com/go-htmx/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	db := config.InitDB()
 	uc := controllers.NewUserController(db)
 	hub := controllers.NewHub()
